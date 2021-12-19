5b7125a35c94eb577dd28d95f3df3e36df8630d8
FujimuraShin <sfujimura2@gmail.com>
2021-12-19 19:32:16 +0900

    Product001

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\OwnersController;
 use App\Http\Controllers\Admin\ShopController;
 use App\Http\Controllers\Admin\ImageController;
+use App\Http\Controllers\Admin\ProductController;
 
 /*
 |--------------------------------------------------------------------------
